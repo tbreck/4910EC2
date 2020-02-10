f5a24882c1c84a4b2ff427a2ad2de1b80e512e0e
tbreck <36282848+tbreck@users.noreply.github.com>
2020-02-10 00:46:53 -0500

    introducing login/registration files

---
+++ api.php
@@ -0,0 +1,153 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+function getDB(){
+	//file path to json file
+	$dbpath = 'private/database.json';
+
+	//load it
+	$db = file_get_contents($dbpath);
+
+	//if blank, use empty array, else json decode into array
+	if($db === "" || $db === false){
+		$db = Array();
+	}else{
+		$db = json_decode($db,true);
+	}
+	return $db;
+}
+
+function writeDB($db){
+	//file path to json file
+	$dbpath = 'private/database.json';
+
+	//write it
+	file_put_contents($dbpath, json_encode($db));
+}
+
+function getUser($username){
+	$db = getDB();
+
+	//the following will print to /var/log/apache2/error.log
+	//you can watch it with
+	//	sudo tail -f /var/log/apache2/error.log 
+	//you'll need to use ctrl+c to exit, it'll stay up forever
+	//error_log("Get user $username from ".json_encode($db));
+
+	//check if user exists
+	if(array_key_exists($username, $db)){
+		return $db[$username];
+	}
+
+	return false;
+}
+
+function saveUser($user){
+	$db = getDB();
+
+	//another way to check for key in array
+	if(isset($db[$user['username']])){
+		return "User exists";
+	}
+
+	//write obj into users array (db array)
+	$db[$user['username']] = $user;
+
+	//save to disk
+	writeDB($db);
+
+	return true;
+}
+
+function authUser($username,$password){
+	$db = getDB();
+	if(!isset($db[$username])){
+		return "No User";
+	}
+	if($password !== $db[$username]['password']){
+		return "No Match";
+	}
+
+	return true;
+}
+
+//create the slim app
+$app = new \Slim\App;
+
+//because of our rewrite rule in .htaccess
+//any HTTP request for a path inside of this folder that doesnt match a file or directory
+//will instead invoke this file
+//Slim can read from apache what the HTTP request was, and will use that info
+//like method and path, to call one of these functions
+
+//listen for a GET against anything like /users/<username>
+//which is relative to the dir its in, so really probably /proj3/users/<username>
+$app->get('/users/{name}', function (Request $request, Response $response, array $args) {
+	//slim knows that {name} is a variable and pulls it out of the URL for you
+	//its in $args
+	$name = $args['name'];
+	$user = getUser($name);
+	if($user === false){
+
+		//if user doesn't exist, return a 404
+		return $response->withStatus(404);
+	}
+
+	//if user does exist, return a 200
+	//we could eventually return the whole user object here if we wanted to
+	return $response->withStatus(200)->getBody()->write("exists");
+});
+
+//listen for a POST to /users
+$app->post('/users', function (Request $request, Response $response, array $args) {
+
+	//php was built to be able to natively decode HTML <form> variables,
+	//since the action was post on the form, not only does slim know to call this function
+	//but php knows how to extract the variables and stores them inside $_POST (global variable)
+	$user = array(
+		'username' => $_POST['username'],
+		'password' => $_POST['password'],
+		'name' => $_POST['name']
+	);
+
+	$result = saveUser($user);
+
+	//the form that posted to this endpoint is waiting for a response
+	//unlike when using the axios library, this is a full page load / redirection
+	//a 302 would not work inside /users/{name}, because its just JS doing the request, not the
+	//browser window
+
+	//if it worked and saved...
+	if($result === true){
+		return $response->withRedirect('login.html', 302);
+	}
+	//else user the # to pass an error back to the browser and reload the same page they came from
+	return $response->withRedirect('registration.html#'.$result, 302);
+});
+
+$app->post('/auth', function (Request $request, Response $response, array $args) {
+	//create a session, or load an existing session from memory
+	session_start();
+
+	//attempt to verify (authenticate) user
+	$result = authUser($_POST['username'], $_POST['password']);
+	if($result === true){
+		//it worked, save username and name into session memory for later use
+		$_SESSION['username'] = $_POST['username'];
+		$_SESSION['name'] = getUser($_POST['username'])['name'];
+
+		//direct user back to index.php
+		return $response->withRedirect('index.php', 302);
+	}
+	//else it didnt work, kill the session.
+	session_destroy();
+	//and send them back to the login page with a message.
+	return $response->withRedirect('login.html#'.$result, 302);
+});
+$app->run();
+
+
+?>
---
+++ registration.js
@@ -0,0 +1,189 @@
+let strengthLabels = [
+	"Very Weak",
+	"Weak",
+	"Fair",
+	"Strong",
+	"Very Strong"
+];
+
+let usernameExists = false;
+
+let usernameCheck = (username) => {
+	//when the button calls this it passes in an event object
+	//when the validation code calls this we get a value (String)
+	if(typeof username === "object"){
+		username = document.getElementById("username").value;
+	}
+
+	//define our response handler
+	let handler = (response) => {
+		console.log('handled',response)
+		if(response.status === 404){
+			usernameExists = false;
+			document.getElementById('usernameStatus').innerHTML = "done";
+		}else{
+			usernameExists = true;
+			document.getElementById('usernameStatus').innerHTML = "error";
+		}
+	}
+
+	//make a GET request to users/<username>,
+	//accept a valid status of 200 or 404, anything else will throw a console error
+	//we are not registering an error handler so errors should bubble up to top of app
+	axios.get('users/'+username,{
+		validateStatus: (status) => status === 200 || status === 404
+	})
+		.then(handler);
+};
+
+let strengthCheck = (password) => {
+	let msg = document.getElementById('passwordmessage');
+	
+	//when the button calls this it passes in an event object
+	//when the validation code calls this we get a value (String)
+	if(typeof password === "object"){
+		password = document.getElementById("password").value;
+	}
+
+	if(password === ""){
+		msg.innerHTML = ' ';
+		return;
+	}
+
+	//Calculate the strength of the password.
+	let result = zxcvbn(password,[]);
+	let score = result.score;
+
+	msg.innerHTML = '<strong>'+strengthLabels[score]+'</strong> Password';		
+	//for debugging: 
+	console.log('strength score is: '+score);
+	return score;
+};
+
+
+let showError = (error) => {
+	let errorNode = document.getElementById("error");
+	if(error === ""){
+		//clear error
+		console.log('clearing error');
+		errorNode.style.display="none";
+		errorNode.innerHTML = "<p></p>";
+	}else{
+		//set error
+		console.log('setting error "'+error+'"');
+		errorNode.style.display="block";
+		errorNode.innerHTML = "<p>"+error+"</p>";
+		errorNode.focus();
+	}
+};
+
+//these are functions returning functions
+//config settings get passed into the outter function
+//and the value to test against is passed into the inner function
+//they use a scope closure to hold onto the config settings
+//because the inner function was created in a scope where 'message' exists, for example
+//passing a test returns true, failing a test returns a message
+let minLength = (length, message) => 
+	(value) => value.length > length ? true : message; 
+let maxLength = (length, message) => 
+	(value) => value.length < length ? true : message; 
+let regex = (regex,message) => 
+	(value) => regex.test(value) ? true : message;
+
+let strength = (strength,message) => 
+	(value) => strengthCheck(value) >= strength ? true : message;
+let confirmed = (message) =>
+	(value) => document.getElementById("confirm_password").value === value ? true : message;
+let available = (message) => 
+	() => !usernameExists ? true : message;
+
+
+//now we compose the above validation building blocks
+//into something thats easy to read and adjust
+//each top level key is a domNode id, like username, and points to an array,
+//each array contains function pointers
+//each function pointer is a closure'd inner-function from above
+//and accepts a value to test against
+let validation = {
+	username: [ //the array of tests to run against usernames
+		minLength(3, "Username too short"),
+		maxLength(10, "Username too long"),
+		regex(/^[0-9a-zA-Z]+$/,"Username can only be letters and numbers"),
+		available("Username exists")
+	],
+	password: [
+		minLength(1,"Password too short"),
+		confirmed("Passwords must match"),
+		strength(2,"Password not strong enough")
+	],
+	name: [
+		minLength(1,"Name too short"),
+	]
+};
+
+//given a dom element id, run each appropriate validation test
+//against the value contained in the element id
+let validate = (id) => {
+	let value = document.getElementById(id).value;
+	let tests = validation[id];
+	for(let i=0;i<tests.length;i++){
+		//execute the test, the inner fctn from above, (like minLength) on the value
+		let result = tests[i](value);
+		if(result !== true){
+			return result;
+		}
+	}
+	return true;
+}
+
+let submitForm = function(e){
+	let ids = ['username','password','name'];
+
+	for(let i = 0;i<ids.length;i++){
+		let id = ids[i];
+		let result = validate(id);
+		if(result !== true){
+			showError(result);
+
+			//stop form from doing it's action
+			e.preventDefault();
+			return false;
+		}
+	}
+	return true;
+};
+
+window.addEventListener("load",function(){
+	//listen for all types of changes to password field
+	//so we can update the strength
+	let passwordNode = document.getElementById('password');
+	passwordNode.addEventListener('keydown',strengthCheck);
+	passwordNode.addEventListener('keyup',strengthCheck);
+	passwordNode.addEventListener('cut',strengthCheck);
+	passwordNode.addEventListener('paste',strengthCheck);
+	passwordNode.addEventListener('blur',strengthCheck);
+
+	//listen for all types of changes to username field
+	//so we can update the icon and exists boolean
+	let usernameNode = document.getElementById('username');
+	usernameNode.addEventListener('keydown',usernameCheck);
+	usernameNode.addEventListener('keyup',usernameCheck);
+	usernameNode.addEventListener('cut',usernameCheck);
+	usernameNode.addEventListener('paste',usernameCheck);
+	usernameNode.addEventListener('blur',usernameCheck);
+
+	//register submit handler
+	let formNode = document.getElementById('form');
+	formNode.addEventListener('submit',submitForm);
+
+	//grab any error messages returned by php in the # field
+	let errorMessage = window.location.hash;
+	window.location.hash = "";
+	console.log('error message is'+errorMessage);
+
+	//if an error from php exists, show it
+	if(errorMessage && errorMessage.length > 0){
+		errorMessage = decodeURIComponent(errorMessage).slice(1);
+		showError(errorMessage);
+	}
+});
