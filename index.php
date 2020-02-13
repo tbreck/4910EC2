d574fd7b7e9165f58e8bcce728cba44f94322dc6
tbreck <36282848+tbreck@users.noreply.github.com>
2020-02-13 17:02:53 -0500

    Changed Name

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<title>Project 3</title>
-<h1>Project 3</h1>
+<title>CPSC 4910</title>
+<h1>Team 10 - Catalog</h1>
 <section>
 	<p>
 <?php
 if(isset($_SESSION['name'])){
-		echo "Hi ".$_SESSION['name']."! ";
+		echo "Hi ".$_SESSION['name']."! \n\n\n";
 	}
 ?>
 	Go <a href="login.html">here to login</a> or <a href="registration.html">here to register</a>.
