9bfc95820ed746e01f9a73a0853e8b2cd6a255c1
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-02 14:58:47 -0500

    Update testdoc.php

--- testdoc.php
+++ testdoc.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Driver_ID, First_Name, Last_Name FROM TestDB.Driver ORDER BY Driver_ID ASC";
+$sql = "SELECT Driver_ID, Email, First_Name, Last_Name FROM TestDB.Driver ORDER BY Driver_ID ASC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Driver_ID: " . $row["Driver_ID"]. " - Name: " . $row["First_Name"]. " " . $row["Last_Name"]. "<br>";
+        echo "Driver_ID: " . $row["Driver_ID"] . $row["Email"]. " - Name: " . $row["First_Name"]. " " . $row["Last_Name"]. "<br>";
     }
 } else {
     echo "0 results";
