95e25be90110178bf6ee4e5c6fe5cf7bb140a9ff
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-02 14:36:33 -0500

    Update testdoc.php

--- testdoc.php
+++ testdoc.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT email, First_Name, Last_Name FROM Driver";
+$sql = "SELECT Driver_ID, First_Name, Last_Name FROM Driver";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "email: " . $row["email"]. " - Name: " . $row["email"]. " " . $row["email"]. "<br>";
+        echo "Driver_ID: " . $row["Driver_ID"]. " - Name: " . $row["Driver_ID"]. " " . $row["Driver_ID"]. "<br>";
     }
 } else {
     echo "0 results";
