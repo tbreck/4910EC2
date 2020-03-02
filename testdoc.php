71abe9cf3e09431b9b70cfc89f24b7d17b2a60e8
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-02 14:56:41 -0500

    Update testdoc.php

--- testdoc.php
+++ testdoc.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Driver_ID: " . $row["Driver_ID"]. " - Name: " . $row["Driver_ID"]. " " . $row["Driver_ID"]. "<br>";
+        echo "Driver_ID: " . $row["Driver_ID"]. " - Name: " . $row["First_Name"]. " " . $row["Last_Name"]. "<br>";
     }
 } else {
     echo "0 results";
