4c7b3a905eddddd2d20fc523625234a327699405
Tanner <tbrecke@clemson.edu>
2020-03-08 22:01:17 -0400

    dasd

--- login/login.php
+++ login/login.php
@@ -1,38 +1,38 @@
 <?php
 
-// $servername = "database-4910.cj8zoecgen2f.us-east-1.rds.amazonaws.com";
-// $username = "admin";
-// $password = "CPSC4910Team10";
+$servername = "database-4910.cj8zoecgen2f.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "CPSC4910Team10";
 
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
 
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-// echo "Connected successfully";
-// console.log("Connected successfully!!!!!");
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+console.log("Connected successfully!!!!!");
 
-// If(isset($_POST['submit'])){
-// 	$Email = $_POST['email']; //echo $email;
-// 	$Password = $_POST['password']; //echo $password;
-// 	echo $Email;
-// 	echo $Password;
+If(isset($_POST['submit'])){
+	$Email = $_POST['email']; //echo $email;
+	$Password = $_POST['password']; //echo $password;
+	echo $Email;
+	echo $Password;
 
-//   $query = "SELECT * FROM TestDB.Administrator WHERE Email ='$Email' AND Password='$Password'";
-// 	$result = mysqli_query($db, $query);
+  $query = "SELECT * FROM TestDB.Administrator WHERE Email ='$Email' AND Password='$Password'";
+	$result = mysqli_query($db, $query);
 
 
-//   if ($result->num_rows > 0) {
-//       // output data of each row
-//       header('Location: date.php');
+  if ($result->num_rows > 0) {
+      // output data of each row
+      header('Location: date.php');
 
-//   } else {
-//       echo "0 results";
-//   }
+  } else {
+      echo "0 results";
+  }
 
-// }
+}
 
 
 ?>
