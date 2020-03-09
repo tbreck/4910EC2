0fc69ba537c8dd98d7d4300dca2df76cf02c469d
Tanner <tbrecke@clemson.edu>
2020-03-08 21:10:29 -0400

    testing

--- login/login.php
+++ login/login.php
@@ -16,6 +16,8 @@ console.log("Connected successfully!!!!!");
 If(isset($_POST['submit'])){
 	$Email = $_POST['Username']; //echo $email;
 	$Password = $_POST['Password']; //echo $password;
+	echo $Email;
+	echo $Password;
 
   $query = "SELECT * FROM TestDB.Administrator WHERE Email ='$Email' AND Password='$Password'";
 	$result = mysqli_query($db, $query);
