6cb6ea2f466de7396d43f9672857435005ddb715
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-10 18:17:54 -0400

    Update login.php

--- login/login.php
+++ login/login.php
@@ -19,19 +19,22 @@ If(isset($_POST['submit'])){
 	//SQL query - match username and password
 	$query = "SELECT * FROM Administrator WHERE Email ='$Email' AND password='$password'";
 	$result = mysqli_query($db, $query);
-	if ( $result > 0 ) {
+	$count = $result->rowCount();
+	if ( $count > 0 ) {
 		header('Location: ../testDatabase/test_admin_page.php');
 	}
 	else{
 		$query = "SELECT * FROM Sponsor WHERE Email ='$Email' AND password='$password'";
 		$result = mysqli_query($db, $query);
-		if ( $result > 0 ) {
+		$count = $result->rowCount();
+		if ( $count > 0 ) {
 			header('Location: ../testDatabase/test_sponsor_page.php');
 		}
 		else{
 			$query = "SELECT * FROM Driver WHERE Email ='$Email' AND password='$password'";
 			$result = mysqli_query($db, $query);
-			if ( $result > 0 ) {
+			$count = $result->rowCount();
+			if ( $count > 0 ) {
 				header('Location: ../testDatabase/test_driver_page.php');
 			}
 			else{
