5236eb946385a48f51ab9cf2dd3f3e7c644079f1
Ben Meadows <btmeado@clemson.edu>
2020-03-24 17:42:49 +0000

    Output array contents and not just data type

--- login/login.php
+++ login/login.php
@@ -14,8 +14,8 @@ If(isset($_POST['submit'])){
     $Email = $_POST['emailID']; //echo $username;
     $Password = $_POST['password']; //echo $password;
 
-    echo $Email;
-    echo $Password;
+    echo $Email; //TEST
+    echo $Password; //TEST
 
     //SQL query - match username and password
     $query = "(SELECT
@@ -46,7 +46,11 @@ If(isset($_POST['submit'])){
     }
 
     $userType = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    echo $userType;
+    echo $userType; //TEST
+
+    foreach($userType['data'] as $userType) {
+	echo $userType['type'], '<br>';
+    }
 /*
     if ( $userType["From_Table"] == "Driver"){
         header('Location: ../testDatabase/test_driver_page.php');
