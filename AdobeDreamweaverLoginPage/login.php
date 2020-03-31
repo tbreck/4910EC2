27aa05d8b26f3aea3421bb651f1808f0a1c8200c
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-30 22:41:24 -0400

    Update login.php

--- AdobeDreamweaverLoginPage/login.php
+++ AdobeDreamweaverLoginPage/login.php
@@ -108,8 +108,8 @@ body {
 
     <div id="LoginPageBox">
 		<h1 id="LoginPageLoginHeader">Login</h1>
-		<input type="text" class="form-control" id="LoginPageUsernameButton" placeholder = "Email">
-		<input type="text" class="form-control" id="LoginoNamePasswordButton" placeholder = "Password">
+		<input type="text" class="form-control" id="emailID" placeholder = "Email">
+		<input type="text" class="form-control" id="password" placeholder = "Password">
 	  <button type="button" id="LoginPageLoginButton">Login</button>
 
 		<button type="button" id="LoginPageForgotButton">Forgot Password?</button>
