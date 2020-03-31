c2dae5f64c0fc20ca3352cc2e44e02bfffd08ec8
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-30 23:23:35 -0400

    Updates
    Changing top nav bars to link to example pages

--- AdobeDreamweaverLoginPage/login.php
+++ AdobeDreamweaverLoginPage/login.php
@@ -86,13 +86,13 @@ body {
               <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Catalog <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfile">Account <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/AdobeDreamweaverForgotPasswordEnterEmailPage/ForgotPasswordEnterEmailPage.html">Account <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
               <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfileSponsor.html">Points <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfileAdmin.html">Settings <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfile.html">Settings <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
               <a class="nav-link" href="/AdobeDreamweaverSettingsPage/SettingsPage.html">Your Cart <span class="sr-only">(current)</span></a>
