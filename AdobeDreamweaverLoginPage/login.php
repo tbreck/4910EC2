2f0b5bbfca66fbd2d49ad5f0b507d4273d361102
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-30 23:29:12 -0400

    Updates

--- AdobeDreamweaverLoginPage/login.php
+++ AdobeDreamweaverLoginPage/login.php
@@ -76,7 +76,7 @@ body {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">TruckTracker&nbsp;</a>
+        <a class="navbar-brand" href="../index.php">TruckTracker&nbsp;</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
