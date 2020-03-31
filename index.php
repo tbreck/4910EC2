b6b6e4c52f9221ad1b73682ff9fc746778f35bee
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-03-31 00:02:24 -0400

    Update

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
               <a class="nav-link" href="/AdobeDreamweaverForgotPasswordEnterEmailPage/ForgotPasswordEnterEmailPage.html">Account <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfileSponsor.html">Points <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/AdobeDreamweaverHomePage/HomeTemplateVersionSponsor.html">Points <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
               <a class="nav-link" href="/AdobeDreamweaverProfilePages/FreshStartProfile.html">Settings <span class="sr-only">(current)</span></a>
