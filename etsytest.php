84f765aef1a5856d91a0aa3b05661d3019bbde11
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-04-06 21:46:23 -0400

    testing Etsy api

---
+++ etsytest.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<title>TruckTracker</title>
+<h1>TruckTracker</h1>
+
+<style>
+	.button {
+	background-color: #1c87c9;
+	border: none;
+	color: white;
+	padding: 20px 34px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 20px;
+	margin: 4px 2px;
+	cursor: pointer;
+	}
+	</style>
+<body>
+ GET https://openapi.etsy.com/v2/listings/active?api_key={aj02728oc69m7wnkyzrgrnup}
+</body>
