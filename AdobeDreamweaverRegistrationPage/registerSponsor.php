a1c4f8b32f71cba189f4bc3147abdc8d09b3a901
Ben Meadows <btmeado@clemson.edu>
2020-04-14 19:58:55 +0000

    Sponsor page registration

--- AdobeDreamweaverRegistrationPage/registerSponsor.php
+++ AdobeDreamweaverRegistrationPage/registerSponsor.php
@@ -1,4 +1,33 @@
+<?php
 
+$db = mysqli_connect('database-4910.cj8zoecgen2f.us-east-1.rds.amazonaws.com', 'admin', 'CPSC4910Team10', 'TestDB')
+or die('Error connecting to MySQL server');
+session_start();
+error_reporting(E_ALL);
+
+?>
+
+<?php
+If(isset($_POST['LoginPageLoginButton'])){
+        $Email = $_POST['LoginPageUsername']; //echo $Email;
+        $CompanyName = $_POST['RegisterDriverFirstName']; //echo $FirstName;
+        $Address = $_POST['RegisterDriverAddress']; //echo $Address;
+        $Password = $_POST['LoginNamePassword']; //echo $Password;
+        $PasswordConfirm = $_POST['LoginNameConfirmPassword'];
+
+        $query = "INSERT INTO Sponsor (Email, Company_Name, Address, Password)
+                  VALUES ('$Email', '$CompanyName', '$Address', '$Password')";
+
+        if(mysqli_query($db, $query) == TRUE){
+                header('Location: ../AdobeDreamweaverHomePage/HomeTemplateVersionSponsor.html'); //echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }else{
+                echo("Registration Failed!");
+                echo "Error: " . $sql . "<br>" .mysqli_error($db);
+        }
+
+}
+
+?>
 
 <html>
 <html lang="en">
@@ -18,26 +47,26 @@ body {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="../index.php">TruckTracker&nbsp;</a>
+        <a class="navbar-brand" href="#">TruckTracker&nbsp;</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Catalog <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Catalog <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Account <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Account <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Points <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Points <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Settings <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Settings <span class="sr-only">(current)</span></a>
             </li>
 			  <li class="nav-item active">
-              <a class="nav-link" href="/AdobeDreamweaverLoginPage/login.php">Your Cart <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Your Cart <span class="sr-only">(current)</span></a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
@@ -50,12 +79,12 @@ body {
 	
     <div id="LoginPageBox">
 		<h1 id="LoginPageLoginHeader">Register for a Sponsor Account!</h1>
-		<input type="text" class="form-control" id="LoginPageUsernameButton" placeholder = "Email">
-		<input type="text" class="form-control" id="RegisterDriverFirstNameButton" placeholder = "Company Name">
-		<input type="text" class="form-control" id="RegisterDriverAddressButton" placeholder = "Address">
-		<input type="text" class="form-control" id="LoginoNamePasswordButton" placeholder = "Password">
-		<input type="text" class="form-control" id="LoginoNameConfirmPasswordButton" placeholder = "Confirm Password">
-		<button type="button" id="LoginPageLoginButton">Submit</button>
+		<input type="text" class="form-control" id="LoginPageUsernameButton" name="LoginPageUsername" placeholder = "Email">
+		<input type="text" class="form-control" id="RegisterDriverFirstNameButton" name="RegisterDriverFirstName" placeholder = "Company Name">
+		<input type="text" class="form-control" id="RegisterDriverAddressButton" name="RegisterDriverAddress" placeholder = "Address">
+		<input type="text" class="form-control" id="LoginoNamePasswordButton" name="LoginNamePassword" placeholder = "Password">
+		<input type="text" class="form-control" id="LoginoNameConfirmPasswordButton" name="LoginNameConfirmPassword" placeholder = "Confirm Password">
+		<button type="submit" id="LoginPageLoginButton">Submit</button>
 	</div>
 	
 <script type="text/javascript" src="js/RegisterSponsorPageBootstrap.js"></script>
