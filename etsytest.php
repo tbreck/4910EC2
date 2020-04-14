9d2eb9ed78cabb797eaa35703a9296cbe01c2420
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-04-13 22:40:51 -0400

    Update etsytest.php

--- etsytest.php
+++ etsytest.php
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+
+
+
+
+
+
+
+
+
+
+<html>
 <title>TruckTracker</title>
 <h1>TruckTracker</h1>
 
@@ -19,28 +29,10 @@
 <body>
 
 <div id="19263084"><div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/525927554/materials-science-coaster-set-lithium?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/68b3b9/1276707717/il_170x135.1276707717_ekgg.jpg?version=1"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/755714591/bubble-chamber-watercolor-coasters?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/71a606/2095520844/il_170x135.2095520844_6rmc.jpg?version=0"></a><center>$30</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/755685273/ooak-nasa-galaxy-technical-diagram?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/c/2802/3000/0/0/il/c5e493/2142959271/il_170x135.2142959271_d21c.jpg"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/502777073/1908-sears-catalog-coaster-set-womens?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/ed7155/1114730924/il_170x135.1114730924_byr9.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/486378951/moon-phases-coaster-set-set-of-4-or-6?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/d59889/1111758409/il_170x135.1111758409_46pj.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/213719681/bubble-chamber-coasters-set-of-5-black?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/b02dd5/739770166/il_170x135.739770166_h95p.jpg?version=0"></a><center>$30</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/476800430/nasa-technical-diagram-coaster-set-aged?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/c/1320/1054/0/67/il/c719ff/1077376966/il_170x135.1077376966_pj9d.jpg"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/277037214/lunar-coaster-set-phases-of-the-moon-set?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/c/1253/1000/129/112/il/a57eb8/1007481497/il_170x135.1007481497_e3me.jpg"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/502778565/1908-sears-catalog-coaster-set-mens?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/c/1122/895/0/255/il/521e5a/1114736932/il_170x135.1114736932_c8sb.jpg"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/479091486/colorful-coaster-set-set-of-4-resin?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/788f48/1084507716/il_170x135.1084507716_l9pl.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/512925271/vintage-mens-fashion-coaster-set-1908?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/45bcc6/1145192074/il_170x135.1145192074_o8gc.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/230045302/nasa-technical-diagram-coaster-set-resin?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/dcd1b5/760877868/il_170x135.760877868_do9f.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/230884223/chemistry-themed-coaster-set-resin-cork?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/b649f8/761483794/il_170x135.761483794_p0gz.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/220661172/biology-coaster-set-resin-and-cork?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/65fb5e/737812290/il_170x135.737812290_24uf.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/230877191/chemistry-lover-coaster-set-resin-cork?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/e86c8e/761572099/il_170x135.761572099_g8iw.jpg?version=1"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/489292317/laboratory-coaster-set-custom-color-hand?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/72c34d/1076730004/il_170x135.1076730004_4g0d.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/460336306/platonic-solids-coasters-blue-chaotic?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/c/1500/1125/0/0/il/ece058/1076250697/il_170x135.1076250697_m2lp.jpg"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/479855263/love-is-love-coaster-set-rainbow-pride?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/9fcc90/1092755239/il_170x135.1092755239_gcqk.jpg?version=0"></a><center>$24</center></div>
-<div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/260341732/sparkly-petri-dish-coasters-purple-teal?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top:15px;padding-left:15px;padding-right15px" src="https://i.etsystatic.com/8809205/d/il/e90f9d/887252389/il_170x135.887252389_7kt1.jpg?version=0"></a><center>$24</center></div></div>
 <div style="display: inline-block"><a target="_blank" href="https://openapi.etsy.com/v2/listings/active?api_key=aj02728oc69m7wnkyzrgrnup"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://www.davidegazze.eu/wp-content/uploads/2019/08/test-740x651.png"></a><center>$24</center></div>
 
 </body>
-
+</html>
 
 
 <!--aj02728oc69m7wnkyzrgrnup-->
