eb4fd91eee2cc6bc73208c4f974d2c397a9ae78f
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-04-13 23:00:25 -0400

    Update etsytest.php

--- etsytest.php
+++ etsytest.php
@@ -25,7 +25,8 @@
 <div id="19263084"><div style="display: inline-block"><a target="_blank" href="https://www.etsy.com/listing/525927554/materials-science-coaster-set-lithium?utm_source=protonpaperiepressli&amp;utm_medium=api&amp;utm_campaign=api"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/68b3b9/1276707717/il_170x135.1276707717_ekgg.jpg?version=1"></a><center>$24</center></div>
 <!--<div style="display: inline-block"><a target="_blank" href="https://openapi.etsy.com/v2/listings/active?api_key=aj02728oc69m7wnkyzrgrnup"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://www.davidegazze.eu/wp-content/uploads/2019/08/test-740x651.png"></a><center>$24</center></div>
 -->
-<div style="display: inline-block"><a target="_blank" href="https://openapi.etsy.com/v2/listings/active?api_key=aj02728oc69m7wnkyzrgrnup"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://openapi.etsy.com/v2/listings/689317981/images/active?api_key=aj02728oc69m7wnkyzrgrnup"></a><center>$24</center></div>
+<div style="display: inline-block"><a target="_blank" href="https://openapi.etsy.com/v2/listings/active?api_key=aj02728oc69m7wnkyzrgrnup"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://www.davidegazze.eu/wp-content/uploads/2019/08/test-740x651.png"></a><center>$24</center></div>
+<div style="display: inline-block"><a target="_blank" href="https://openapi.etsy.com/v2/listings/689317981/images/active?api_key=aj02728oc69m7wnkyzrgrnup"><img class="pullImg" style="padding-top: 15px; padding-left: 15px; opacity: 1;" src="https://i.etsystatic.com/8809205/d/il/68b3b9/1276707717/il_170x135.1276707717_ekgg.jpg?version=1"></a><center>$24</center></div>
 
 
 </body>
