bbc769f8cfe0ea9e3a2734a738caffcdb7292c2f
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-04-26 17:23:17 -0400

    Update sponsorProfile.php

--- AdobeDreamweaverProfilePages/sponsorProfile.php
+++ AdobeDreamweaverProfilePages/sponsorProfile.php
@@ -26,7 +26,7 @@ $_SESSION['Address'] = $rowInfo['Address'];
 $_SESSION['Sponsor_ID'] = $rowInfo['Sponsor_ID'];
 $_SESSION['Point_Dollar_Ratio'] = $rowInfo['Point_Dollar_Ratio'];
 
-$sql = "SELECT  Count(*)
+$sql = "SELECT  Sponsor_ID, Count(*)
         FROM Driver/Sponsor
         WHERE Sponsor_ID = '{$_SESSION['Sponsor_ID']}'";
 $count_drivers = mysqli_query($db, $sql);
