ff0e5db8573a32d2cea9d84462c95486b2114782
dhbeamer <57417411+dhbeamer@users.noreply.github.com>
2020-04-26 18:03:42 -0400

    Update sponsorProfile.php

--- AdobeDreamweaverProfilePages/sponsorProfile.php
+++ AdobeDreamweaverProfilePages/sponsorProfile.php
@@ -109,6 +109,10 @@ body {
   <button type="button">Edit Profile</button>
   <button type="button">Reset Password</button>
 
+  <input type="text" class="form-control" id="DriverEmailtoAdd" placeholder = "Driver's Email To Add To List">
+  <button type="button" id="AddDriver">Add Driver</button>
+
+
 </div>
 
 <script type="text/javascript" src="js/FreshStartBootstrap.js"></script>
